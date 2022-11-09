ead33f0310270f4ebab50f09a3ed9cf0a9ee0333
ChiefAdit <89474855+ChiefAdit@users.noreply.github.com>
2022-11-09 11:56:43 +0700

    Add files via upload

---
+++ pim/frontend/loginadmin.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+   
+  </head>
+  <body>
+
+    <form action="menuadmin.php" method="post">
+    <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Username </span>
+            <input type="text" class="form-control" name="email" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+    </div> 
+    <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">password </span>
+            <input type="password" class="form-control" name="password" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+    </div>  
+      
+      
+      <input type="submit" value="Submit">
+
+
+  </body>
+</html>
